462279d03a0d653f159c0fe169c812ac2fa2f7fe
Julian Spinelli <julespi@gmail.com>
2021-11-05 14:51:18 -0300

    clase 20

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from './client'
-import { CLIENTS } from './clientes.json'
+import { ClientService } from './client.service'
 
 @Component({
   selector: 'app-clients',
@@ -10,10 +10,10 @@ export class ClientsComponent implements OnInit {
 
   clients: Client[];
 
-  constructor() { }
+  constructor(private clientService: ClientService) { }
 
   ngOnInit(): void {
-    this.clients = CLIENTS;
+    this.clients = this.clientService.getClients();
   }
 
 }
