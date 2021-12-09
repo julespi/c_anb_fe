02821e247fd4d135834208e494877905a0c25c54
Julian Spinelli <julespi@gmail.com>
2021-12-09 11:23:50 -0300

    clase 103

--- src/app/clients/detail/detail.component.ts
+++ src/app/clients/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import { Client } from '../client';
 import { ClientService } from '../client.service';
 
@@ -11,7 +11,7 @@ import { ClientService } from '../client.service';
 })
 export class DetailComponent implements OnInit {
   client: Client;
-  title: string = "Client Details";
+  title: string = 'Client Details';
   selectedFile: File;
 
   constructor(
@@ -20,33 +20,46 @@ export class DetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(params => {
-      let id:number = +params.get('id');
-      if(id){
-        this.clientService.getClient(id).subscribe(client => {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let id: number = +params.get('id');
+      if (id) {
+        this.clientService.getClient(id).subscribe((client) => {
           this.client = client;
-        })
+        });
       }
-    })
-
+    });
   }
-  
-  public selectFile(event){
+
+  public selectFile(event) {
     this.selectedFile = event.target.files[0];
+    if(this.selectedFile.type.indexOf('image') < 0){
+      Swal.fire(
+        'File type error',
+        'File type must be an image',
+        'error'
+      );
+      this.selectedFile = null;
+    }
   }
 
-  public uploadFile(){
-    console.log(this.selectedFile);
-    this.clientService.upload(this.selectedFile, this.client.id).subscribe(
-      client => {
-        this.client = client;
-        Swal.fire(
-          'Uploaded!',
-          `File ${this.selectedFile.name} has successfully been uploaded.`,
-          'success'
-        );
-      }
-    )
+  public uploadFile() {
+    if (!this.selectedFile) {
+      Swal.fire(
+        'No file selected!',
+        'Please select an image to upload.',
+        'error'
+      );
+    } else {
+      this.clientService
+        .upload(this.selectedFile, this.client.id)
+        .subscribe((client) => {
+          this.client = client;
+          Swal.fire(
+            'Uploaded!',
+            `File ${this.selectedFile.name} has successfully been uploaded.`,
+            'success'
+          );
+        });
+    }
   }
-
 }
